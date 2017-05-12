671d5c071e6c99e35b7dfec06af02996f532b42a
John Kelvie <john.kelvie@gmail.com>
2017-05-11 19:08:03 -0500

    Updates for Part2 - Next/Previous via Queueing

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 // It is NOT a real database, but can be used for testing, as JavaScript Lambdas tend to live for a few hours
 // Stay tuned for a more sophisticated example that uses DynamoDB
 var stateByUser = {};
-var podcastURL = "https://feeds.soundcloud.com/stream/309340878-user-652822799-episode-010-building-an-alexa-skill-with-flask-ask-with-john-wheeler.mp3";
+
+var podcastFeed = [
+    "https://feeds.soundcloud.com/stream/318108640-user-652822799-episode-012-alexa-skill-certification-with-sameer-lalwanilexa-dev-chat-final-mix.mp3",
+    "https://feeds.soundcloud.com/stream/314247951-user-652822799-episode-011-alexa-smart-home-partner-network-with-zach-parker.mp3",
+    "https://feeds.soundcloud.com/stream/309340878-user-652822799-episode-010-building-an-alexa-skill-with-flask-ask-with-john-wheeler.mp3"
+];
 
 // Entry-point for the Lambda
 exports.handler = function(event, context) {
@@ -21,6 +26,7 @@ SimplePlayer.prototype.handle = function () {
     var requestType = this.event.request.type;
     var userId = this.event.context ? this.event.context.System.user.userId : this.event.session.user.userId;
     var response = null;
+    var lastPlayed = this.load(userId);
 
     // On launch, we tell the user what they can do (Play audio :-))
     if (requestType === "LaunchRequest") {
@@ -29,8 +35,27 @@ SimplePlayer.prototype.handle = function () {
     // Handle Intents here - Play, Pause and Resume is all for now
     } else if (requestType === "IntentRequest") {
         var intent = this.event.request.intent;
+
+        // We assume we start with the first podcast, but check the lastPlayed
+        var podcastIndex = 0;
+        if (lastPlayed) {
+            podcastIndex = parseInt(lastPlayed.request.token);
+        }
+
         if (intent.name === "Play") {
-            this.play(podcastURL, 0);
+            this.queue(podcastFeed[podcastIndex], 0, "REPLACE_ALL", podcastIndex);
+
+        } else if (intent.name === "AMAZON.NextIntent") {
+            // If we have reach the end of the feed, start back at the beginning
+            podcastIndex >= podcastFeed.length - 1 ? podcastIndex = 0 : podcastIndex++;
+
+            this.queue(podcastFeed[podcastIndex], 0, "REPLACE_ALL", podcastIndex);
+
+        } else if (intent.name === "AMAZON.PreviousIntent") {
+            // If we have reached the start of the feed, go back to the end
+            podcastIndex == 0 ? podcastIndex = podcastFeed.length - 1 : podcastIndex--;
+
+            this.queue(podcastFeed[podcastIndex], 0, "REPLACE_ALL", podcastIndex);
 
         } else if (intent.name === "AMAZON.PauseIntent") {
             // When we receive a Pause Intent, we need to issue a stop directive
@@ -38,18 +63,30 @@ SimplePlayer.prototype.handle = function () {
             this.stop();
 
         } else if (intent.name === "AMAZON.ResumeIntent") {
-            var lastPlayed = this.load(userId);
             var offsetInMilliseconds = 0;
             if (lastPlayed !== null) {
                 offsetInMilliseconds = lastPlayed.request.offsetInMilliseconds;
             }
 
-            this.play(podcastURL, offsetInMilliseconds);
+            this.queue(podcastFeed[podcastIndex], offsetInMilliseconds, "REPLACE_ALL", podcastIndex);
         }
+    } else if (requestType === "AudioPlayer.PlaybackNearlyFinished") {
+        // If we have reach the end of the feed, start back at the beginning
+        podcastIndex >= podcastFeed.length - 1 ? podcastIndex = 0 : podcastIndex++;
+
+        // We save off the PlaybackStopped Intent, so we know what was last playing
+        this.queue(podcastFeed[podcastIndex], 0, "ENQUEUE", podcastIndex);
+
+    } else if (requestType === "AudioPlayer.PlaybackStarted") {
+        // We simply respond with true to acknowledge the request
+        this.context.succeed(true);
+
     } else if (requestType === "AudioPlayer.PlaybackStopped") {
         // We save off the PlaybackStopped Intent, so we know what was last playing
         this.save(userId, this.event);
 
+        // We respond with just true to acknowledge the request
+        this.context.succeed(true);
     }
 };
 
@@ -74,7 +111,7 @@ SimplePlayer.prototype.say = function (message, repromptMessage) {
                 }
             }
         }
-    }
+    };
     this.context.succeed(response);
 };
 
@@ -83,7 +120,7 @@ SimplePlayer.prototype.say = function (message, repromptMessage) {
  * @param audioURL The URL to play
  * @param offsetInMilliseconds The point from which to play - we set this to something other than zero when resuming
  */
-SimplePlayer.prototype.play = function (audioURL, offsetInMilliseconds) {
+SimplePlayer.prototype.queue = function (audioURL, offsetInMilliseconds, playBehavior, token) {
     var response = {
         version: "1.0",
         response: {
@@ -91,11 +128,11 @@ SimplePlayer.prototype.play = function (audioURL, offsetInMilliseconds) {
             directives: [
                 {
                     type: "AudioPlayer.Play",
-                    playBehavior: "REPLACE_ALL", // Setting to REPLACE_ALL means that this track will start playing immediately
+                    playBehavior: playBehavior,
                     audioItem: {
                         stream: {
                             url: audioURL,
-                            token: "0", // Unique token for the track - needed when queueing multiple tracks
+                            token: token, // Unique token for the track - needed when queueing multiple tracks
                             expectedPreviousToken: null, // The expected previous token - when using queues, ensures safety
                             offsetInMilliseconds: offsetInMilliseconds
                         }
@@ -120,7 +157,7 @@ SimplePlayer.prototype.stop = function () {
                 }
             ]
         }
-    }
+    };
 
     this.context.succeed(response);
 };
